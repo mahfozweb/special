6654fbb2a8ac344f32dd45efd6ab5b47dd238ea2
mahfozweb <mahfoz81000@gmail.com>
2025-08-30 23:16:37 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+document.getElementById("img-change").addEventListener("click", function(){
+         
+    const img = document.getElementById("img-change")
+    img.src = "./Assets/mj.jpg"
+         
+         
+})
